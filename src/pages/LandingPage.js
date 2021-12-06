172420f715493f22ea28b142d55572a6d65f9f3d
abdoulrasheed <abdulrasheedibrahim47@gmail.com>
2021-12-06 15:54:49 +0100

    Initial commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import styles from "../styles/LandingPage.module.css";
+
+const LandingPage = ({ history }) => {
+    return <div className={styles.container}>
+        <div className={styles.mainContainer}>
+            <div className={styles.left}></div>
+            <div className={styles.right}>
+                <div>
+                    <h3 className={styles.title}>Verify Your Identity</h3>
+                    <h5 className={styles.subtitle}>To continue verifying your identity, click on the button below</h5>
+                </div>
+                <a href="google.com/" className={styles.primaryAction}>Begin Verification</a>
+                <h1 className={styles.brandSmall}>from Zarttech</h1>
+            </div>
+        </div>
+        <div className={styles.footerContainer}>
+            <div>
+                <h2 className={styles.brand}>Zarttech Identity Verification</h2>
+                <h2 className={styles.copyright}>Copyright &copy; 2022 Zarttech</h2>
+            </div>
+            <div className={styles.footerItems}>
+                <div>
+                    <li><a href="#doc">Cases</a></li>
+                    <li className={styles.li}><a href="#doc">Documentation</a></li>
+                </div>
+                <div>
+                    <li><a href="#doc">Support</a></li>
+                    <li className={styles.li}><a href="#doc">About Us</a></li>
+                </div>
+                <div>
+                    <li><a href="#doc">Privacy</a></li>
+                    <li className={styles.li}><a href="#doc">Trust and Safety</a></li>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default LandingPage;
